01a262d36f00fe5aa26df1befb656d0b430bba0c
Ishan Kumar <ishan.k@tadigital.com>
2020-11-09 16:56:39 +0530

    Added MongoDB connection to the App

--- server/app.js
+++ server/app.js
@@ -3,6 +3,15 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema.js');
 const app = express();
 
+// connect to mlab database
+const mongoose = require('mongoose');
+mongoose.connect(
+  'mongodb+srv://admin:admin@cluster0.de4wt.mongodb.net/graphql-books?retryWrites=true&w=majority'
+);
+mongoose.connection.once('open', () => {
+  console.log('conneted to database');
+});
+
 app.use(
   '/graphql',
   graphqlHTTP({
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,20 +5,25 @@ const {
   GraphQLString,
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema,
 } = graphql;
 
 var books = [
-  { name: 'xyz', id: '1', genre: 'xyz' },
-  { name: 'xyz2', id: '2', genre: 'xyz' },
-  ,
-  { name: 'xy3', id: '3', genre: 'xyz' },
+  { name: 'xyz', id: '1', genre: 'xyz', authorId: '1' },
+  { name: 'xyz2', id: '2', genre: 'xyz', authorId: '2' },
+  { name: 'xy3', id: '3', genre: 'xyz', authorId: '3' },
+  { name: 'xyz4', id: '4', genre: 'xyz', authorId: '1' },
+  { name: 'xyz5', id: '5', genre: 'xyz', authorId: '2' },
+  { name: 'xyz6', id: '6    ', genre: 'xyz', authorId: '3' },
 ];
 var authors = [
   { name: 'a1', id: '1', age: 22 },
   { name: 'a2', id: '2', age: 40 },
-  ,
   { name: 'a3', id: '3', age: 50 },
+  { name: 'a4', id: '1', age: 22 },
+  { name: 'a5', id: '2', age: 40 },
+  { name: 'a6', id: '3', age: 50 },
 ];
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -28,6 +33,12 @@ const BookType = new GraphQLObjectType({
     },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
   }),
 });
 const AuthorType = new GraphQLObjectType({
@@ -38,6 +49,12 @@ const AuthorType = new GraphQLObjectType({
     },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 const RootQuery = new GraphQLObjectType({
@@ -56,6 +73,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(authors, { id: args.id });
+      },
+    },
+    books: {
+      type: GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      },
+    },
+    authors: {
+      type: GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       },
     },
   },
